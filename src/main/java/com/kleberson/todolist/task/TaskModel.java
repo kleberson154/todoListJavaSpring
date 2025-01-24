ad18f290917cf9be050aa4a482ec45ad770136b9
kleberson154 <kleberson55@hotmail.com>
2025-01-24 09:45:26 -0300

    creating a task table

---
+++ src/main/java/com/kleberson/todolist/task/TaskModel.java
@@ -0,0 +1,27 @@
+package com.kleberson.todolist.task;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID userId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
