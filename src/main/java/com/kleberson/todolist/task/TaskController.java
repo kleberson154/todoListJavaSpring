f9c203101515b33a7fa4e34acbbf0ad271d82fb7
kleberson154 <kleberson55@hotmail.com>
2025-01-29 14:06:27 -0300

    hours validation

--- src/main/java/com/kleberson/todolist/task/TaskController.java
+++ src/main/java/com/kleberson/todolist/task/TaskController.java
@@ -2,11 +2,15 @@ package com.kleberson.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @RestController
@@ -16,8 +20,16 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/create")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         taskModel.setUserId((UUID) request.getAttribute("idUser"));
-        return this.taskRepository.save(taskModel);
+        LocalDateTime date = LocalDateTime.now();
+        if (date.isAfter(taskModel.getStartAt()) || date.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task start date or end date cannot be before current date");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task start date cannot be before end date");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(taskModel));
     }
 }
